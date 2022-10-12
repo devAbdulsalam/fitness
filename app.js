306a127f3e95c8b79d06d34b4a7b14dbcbaaed36
my-amtech <ammuftau74@gmail.com>
2022-10-12 13:06:50 +0100

    First commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var MenuItems = document.getElementById("MenuItems");
+// var MenuItems = document.getElementById("MenuItems");
 MenuItems.style.maxHeight = "0px";
 
 function menutoggle(){
     if(MenuItems.style.maxHeight == "0px")
     {
-        MenuItems.style.maxHeight = "200px";
+        MenuItems.style.maxHeight = "250px";
     }
     else 
     {
